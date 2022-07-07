77456bc5f74131fbcc8f53afb0b0497c72c8167b
randreu28 <randreu28@gmail.com>
2022-07-07 19:06:19 +0200

    Configures redirect according to cookie
    This will allow to save a preference on what portal should be loaded when accessing the root path (business or particular)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "cookie",
+            key: "isBusiness",
+            value: "true",
+          },
+        ],
+        permanent: false,
+        destination: "/empresa",
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "cookie",
+            key: "isBusiness",
+            value: "false",
+          },
+        ],
+        permanent: false,
+        destination: "/particular",
+      },
+      {
+        source: "/",
+        permanent: false,
+        destination: "/particular",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.jsx
+++
@@ -1,3 +0,0 @@
-export default function index() {
-  return <h1 className="text-5xl font-bold">Hello world</h1>;
-}
