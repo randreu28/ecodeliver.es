fbe415fe3ab5c174cbcd20f3b9018986cb7769fe
randreu28 <randreu28@gmail.com>
2022-07-08 17:35:57 +0200

    Updates navbar to accept sections as props

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,9 +5,8 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Logo from "./Logo";
 
-export default function Navbar() {
+export default function Navbar({ navData }) {
   const [section, setSection] = useState("inicio");
-
   useEffect(() => {
     const sections = document.querySelectorAll("section");
     window.addEventListener("scroll", () => {
@@ -29,56 +28,21 @@ export default function Navbar() {
       <div className="flex-row space-x-8 flex">
         <Logo />
         <ul className="my-auto space-x-8 hidden lg:inline">
-          <a
-            href="#inicio"
-            className={
-              section === "inicio"
-                ? "text-primary duration-200 hover:opacity-50"
-                : " duration-200 hover:opacity-50"
-            }
-          >
-            Inicio
-          </a>
-          <a
-            href="#como-funciona"
-            className={
-              section === "como-funciona"
-                ? "text-primary duration-200 hover:opacity-50"
-                : " duration-200 hover:opacity-50"
-            }
-          >
-            Cómo funciona
-          </a>
-          <a
-            href="#valores"
-            className={
-              section === "valores"
-                ? "text-primary duration-200 hover:opacity-50"
-                : " duration-200 hover:opacity-50"
-            }
-          >
-            Valores
-          </a>
-          <a
-            href="#noticias"
-            className={
-              section === "noticias"
-                ? "text-primary duration-200 hover:opacity-50"
-                : " duration-200 hover:opacity-50"
-            }
-          >
-            Noticias
-          </a>
-          <a
-            href="#faq"
-            className={
-              section === "faq"
-                ? "text-primary duration-200 hover:opacity-50"
-                : " duration-200 hover:opacity-50"
-            }
-          >
-            FAQ
-          </a>
+          {navData.map((currentSection) => {
+            return (
+              <a
+                href={"#" + currentSection.href}
+                key={currentSection.name}
+                className={
+                  section === currentSection.href
+                    ? "text-primary duration-200 hover:opacity-50"
+                    : " duration-200 hover:opacity-50"
+                }
+              >
+                {currentSection.name}
+              </a>
+            );
+          })}
         </ul>
       </div>
       <div className="my-auto divide-x divide-gray-300 hidden lg:inline">
@@ -147,61 +111,22 @@ export default function Navbar() {
               </div>
               <div className="px-2 pt-2 pb-3 space-y-1 divide-y">
                 <div>
-                  <Popover.Button
-                    className={
-                      section === "inicio"
-                        ? "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300 text-primary"
-                        : "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300"
-                    }
-                    href="#inicio"
-                    as="a"
-                  >
-                    Inicio
-                  </Popover.Button>
-                  <Popover.Button
-                    className={
-                      section === "como-funciona"
-                        ? "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300 text-primary"
-                        : "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300"
-                    }
-                    href="#como-funciona"
-                    as="a"
-                  >
-                    Cómo funciona
-                  </Popover.Button>
-                  <Popover.Button
-                    className={
-                      section === "valores"
-                        ? "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300 text-primary"
-                        : "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300"
-                    }
-                    href="#valores"
-                    as="a"
-                  >
-                    Valores
-                  </Popover.Button>
-                  <Popover.Button
-                    className={
-                      section === "noticias"
-                        ? "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300 text-primary"
-                        : "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300"
-                    }
-                    href="#noticias"
-                    as="a"
-                  >
-                    Noticias
-                  </Popover.Button>
-                  <Popover.Button
-                    className={
-                      section === "faq"
-                        ? "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300 text-primary"
-                        : "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300"
-                    }
-                    href="#faq"
-                    as="a"
-                  >
-                    FAQ
-                  </Popover.Button>
+                  {navData.map((currentSection) => {
+                    return (
+                      <Popover.Button
+                        key={currentSection.name}
+                        className={
+                          section === currentSection.href
+                            ? "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300 text-primary"
+                            : "block text-lg font-medium px-3 py-2 hover:opacity-75 duration-300"
+                        }
+                        href={"#" + currentSection.href}
+                        as="a"
+                      >
+                        {currentSection.name}
+                      </Popover.Button>
+                    );
+                  })}
                 </div>
 
                 <div>
