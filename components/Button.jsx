e1d90cb57bfec6472eb86cee304b613da394a23f
randreu28 <randreu28@gmail.com>
2022-07-12 20:54:46 +0200

    Enhances button component
    Now it admits events

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,8 +1,9 @@
-export default function Button({ className, text }) {
+export default function Button({ className, text, event }) {
   return (
     <button
+      onClick={event}
       className={
-        "px-4 py-2 font-semibold tracking-wide text-white capitalize transition-colors duration-200 transform bg-primary rounded-md focus:ring focus:ring-green-300 focus:ring-opacity-80 fo sm:mx-2 hover:bg-green-500 focus:outline-none focus:bg-green-600 " +
+        "px-4 py-2 font-semibold tracking-wide text-lg text-white capitalize transition-colors duration-200 transform bg-primary rounded-md focus:ring focus:ring-green-300 focus:ring-opacity-80 fo sm:mx-2 hover:bg-green-500 focus:outline-none focus:bg-green-600 " +
         className
       }
     >
