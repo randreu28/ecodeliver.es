6462e58ee1031e219745e794f8661e549be59385
randreu28 <randreu28@gmail.com>
2022-07-22 21:27:34 +0200

    Implements GA-4
    Fixes #5

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import "../styles/globals.css";
+import { GoogleAnalytics, usePageViews, event } from "nextjs-google-analytics";
+
+export function reportWebVitals({ id, name, label, value }) {
+  event(name, {
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  usePageViews();
+
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
