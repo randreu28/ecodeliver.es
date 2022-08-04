52aeabcd369006eb1833b7a3168aa41a2870968c
randreu28 <randreu28@gmail.com>
2022-08-04 21:44:38 +0200

    Adds className prop to Value component

--- components/Value.jsx
+++ components/Value.jsx
@@ -1,6 +1,11 @@
-export default function Value({ children, title, text }) {
+export default function Value({ children, title, text, className }) {
   return (
-    <div className="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl">
+    <div
+      className={
+        "flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl " +
+        className
+      }
+    >
       <span className="inline-block p-3 text-green-500 bg-green-100 rounded-full">
         {children}
       </span>
