9211494acc1811f75561f47ba4e2e622e77cf012
randreu28 <randreu28@gmail.com>
2022-08-27 19:45:58 +0200

    Creates a draft for TrackYourParcel component
    Related to #22

--- pages/rastrea-tu-paquete/[...orderNumber].jsx
+++ pages/rastrea-tu-paquete/[...orderNumber].jsx
@@ -1,6 +1,6 @@
-import WebHolder from "../../components/Webholder";
 import SmNavbar from "../../components/SmNavbar";
 import Button from "../../components/Button";
+import Step from "../../components/Step";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { useRef } from "react";
 import { useRouter } from "next/router";
@@ -8,10 +8,58 @@ import Image from "next/image";
 
 export default function Tracking({ data, slug }) {
   if (data[0]) {
-    return <WebHolder />;
+    return <TrackYourParcel slug={slug} data={data} />;
   } else {
     return <NotFound slug={slug} />;
   }
+}
+
+function TrackYourParcel({ slug, data }) {
+  return (
+    <>
+      <SmNavbar className="sticky" />
+      <h1 className="text-center font-bold text-3xl">Rastrea tu paquete</h1>
+      <div className="m-5 p-5 sm:m-10 sm:p-10 shadow-lg rounded-xl border border-gray-200">
+        <div className="flex flex-col sm:flex-row gap-4 justify-between text-md">
+          <b className="my-auto text-xl text-center sm:text-left">{slug}</b>
+          <button className="px-4 py-2 font-semibold text-white capitalize transition-colors duration-200 transform bg-primary rounded-md focus:ring focus:ring-green-300 focus:ring-opacity-80 hover:bg-green-500 focus:outline-none focus:bg-green-600">
+            Modificar Entrega
+          </button>
+        </div>
+        <hr className="my-10" />
+        <ol className="flex flex-col space-y-10">
+          <Step title="Preadmitido" date="2 de Agosto, 2022 · 9:16">
+            <h1>The description goes here</h1>
+            <p>Lorem ipsum siamet dolorum ipsamet retorum licatrosu</p>
+          </Step>
+          <Step
+            state="done"
+            title="En reparto"
+            date="3 de Agosto, 2022 · 12:22"
+          >
+            <h1>The description goes here</h1>
+            <p>Lorem ipsum siamet dolorum ipsamet retorum licatrosu</p>
+          </Step>
+          <Step
+            state="doing"
+            title="Entregado"
+            date="3 de Agosto, 2022 · 16:41"
+          >
+            <h1>The description goes here</h1>
+            <p>Lorem ipsum siamet dolorum ipsamet retorum licatrosu</p>
+          </Step>
+          <Step
+            state="warning"
+            title="En espera"
+            date="3 de Agosto, 2022 · 15:48"
+          >
+            <h1>The description goes here</h1>
+            <p>Lorem ipsum siamet dolorum ipsamet retorum licatrosu</p>
+          </Step>
+        </ol>
+      </div>
+    </>
+  );
 }
 
 function NotFound({ slug }) {
