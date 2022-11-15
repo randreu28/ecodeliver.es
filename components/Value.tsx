8d3d1cacab4f9f262ebacd12e2153612a4489380
randreu28 <randreu28@gmail.com>
2022-11-15 12:54:59 +0100

    Migrates Value component

---
+++ components/Value.tsx
@@ -0,0 +1,28 @@
+import { PropsWithoutRef } from "react";
+interface Props {
+  children: JSX.Element;
+  title: string;
+  text: string;
+  className: string;
+}
+
+export default function Value({ children, title, text, className }: Props) {
+  return (
+    <div
+      className={
+        "flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl " +
+        className
+      }
+    >
+      <span className="inline-block p-3 text-green-500 bg-green-100 rounded-full">
+        {children}
+      </span>
+
+      <h1 className="text-2xl font-semibold text-secondary capitalize ">
+        {title}
+      </h1>
+
+      <p className="text-gray-500">{text}</p>
+    </div>
+  );
+}
