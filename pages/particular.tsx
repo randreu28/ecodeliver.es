e7d4feac9e0275527e5371b50bb4332679e8a1c0
randreu28 <randreu28@gmail.com>
2022-11-16 10:51:13 +0100

    Migrates Como funciona section

--- pages/particular.tsx
+++ pages/particular.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef, useState } from "react";
 import Button from "../components/Button";
 import CloudLogo from "../components/CloudLogo";
 import toast, { Toaster } from "react-hot-toast";
+import EcoDriverForm from "../components/EcoDriverForm";
 
 const navData = [
   { name: "Inicio", href: "inicio" },
@@ -113,6 +114,161 @@ export default function Particular() {
             Con el apoyo de...
           </h1>
           <CloudLogo />
+        </div>
+      </section>
+
+      <section id={navData[1].href} className="bg-green-50 space-y-10">
+        <div className="container px-6 py-10 mx-auto">
+          <div className="lg:flex lg:items-center">
+            <div className="w-full space-y-12 lg:w-1/2" data-aos="fade-right">
+              <div>
+                <p className="text-primary font-semibold pb-1">
+                  Nuestro modelo
+                </p>
+                <h1 className="text-3xl font-semibold text-gray-800 lg:text-4xl pb-3">
+                  ¿Cómo funciona?
+                </h1>
+                <p className="text-gray-500 text-xl">
+                  Cualquiera que vaya a viajar entre ciudades puede ser un{" "}
+                  <b>
+                    <span className="text-primary">eco</span>Driver
+                  </b>
+                  , aprovechando el espacio libre en el coche, transportando
+                  paquetes consigo y cubriendo los gastos del viaje!
+                </p>
+                <div>
+                  <span className="inline-block w-40 h-1 rounded-full bg-green-500" />
+                  <span className="inline-block w-3 h-1 ml-1 rounded-full bg-green-500" />
+                  <span className="inline-block w-1 h-1 ml-1 rounded-full bg-green-500" />
+                </div>
+              </div>
+              <div className="md:flex md:items-start md:-mx-4">
+                <span className="inline-block p-2 text-green-500 bg-green-200 rounded-xl md:mx-4">
+                  <p className="px-2 font-bold text-xl">1</p>
+                </span>
+                <div className="mt-4 md:mx-4 md:mt-0">
+                  <h1 className="text-2xl font-semibold text-secondary">
+                    Publica tu viaje en la App
+                  </h1>
+                  <p className="mt-3 text-gray-500">
+                    Tienes planeado viajar en coche? Completa tu perfil e
+                    indícanos la información de tu viaje en la app de{" "}
+                    <b>
+                      <span className="text-primary">eco</span>Deliver
+                    </b>
+                    .
+                  </p>
+                </div>
+              </div>
+              <div className="md:flex md:items-start md:-mx-4">
+                <span className="inline-block p-2 text-green-500 bg-green-200 rounded-xl md:mx-4">
+                  <p className="px-2 font-bold text-xl">2</p>
+                </span>
+                <div className="mt-4 md:mx-4 md:mt-0">
+                  <h1 className="text-2xl font-semibold text-secondary">
+                    Recoge los paquetes en nuestro almacén
+                  </h1>
+                  <p className="mt-3 text-gray-500 ">
+                    Te estaremos esperando con bolsas de paquetes a la salida de
+                    la ciudad.
+                  </p>
+                </div>
+              </div>
+              <div className="md:flex md:items-start md:-mx-4">
+                <span className="inline-block p-2 text-green-500 bg-green-200 rounded-xl md:mx-4">
+                  <p className="px-2 font-bold text-xl">3</p>
+                </span>
+                <div className="mt-4 md:mx-4 md:mt-0">
+                  <h1 className="text-2xl font-semibold text-secondary">
+                    Deja los paquetes al llegar a tu destino
+                  </h1>
+                  <p className="mt-3 text-gray-500">
+                    Cuando llegues a tu destino, dejas las bolsas con paquetes
+                    en nuestro almacén a la entrada de la ciudad y listo! Son 5
+                    minutos de desvío en cada ciudad.
+                  </p>
+                </div>
+              </div>
+              <div className="md:flex md:items-start md:-mx-4">
+                <span className="inline-block p-2 text-green-500 bg-green-200 rounded-xl md:mx-4">
+                  <p className="px-2 font-bold text-xl">4</p>
+                </span>
+                <div className="mt-4 md:mx-4 md:mt-0">
+                  <h1 className="text-2xl font-semibold text-secondary">
+                    Recibe el pago y ¡listo!
+                  </h1>
+                  <p className="mt-3 text-gray-500">
+                    Cubriendo los costes del viaje y evitando que un camión
+                    tenga que hacer el transporte (ahorrando todas esas
+                    emisiones!)
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="hidden lg:flex lg:items-center lg:w-1/2 lg:justify-center"
+              data-aos="fade-left"
+            >
+              <Image
+                className="w-[28rem] h-[28rem] object-cover xl:w-[34rem] xl:h-[34rem] rounded-full"
+                src="/media/stockImage2.jpg"
+                alt=""
+                height={500}
+                width={500}
+              />
+            </div>
+          </div>
+        </div>
+        {/*  eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/media/brand/line-3.svg"
+          className="hidden xl:block absolute translate-x-1/3 -translate-y-40 right-0 h-1/6 scale-x-[-1]"
+          alt=""
+        />
+        <div data-aos="fade-in">
+          <div className="p-5 flex flex-col lg:flex-row rounded-lg w-fit shadow-lg mx-auto pt-10 bg-white gap-20">
+            <div className="p-5 space-y-5">
+              <h1 className="text-4xl lg:text-5xl font-bold text-center text-primary">
+                15.7 kgCO<sub>2</sub>
+              </h1>
+              <p className="text-center text-gray-500 ">
+                Ahorrados en un viaje
+                <br /> Barcelona-Madrid*
+              </p>
+            </div>
+            <div className="p-5 space-y-5">
+              <h1 className="text-4xl lg:text-5xl font-bold text-center text-primary">
+                ~50€
+              </h1>
+              <p className="text-center text-gray-500">
+                De remuneración en un viaje <br />
+                Barcelona-Valencia
+              </p>
+            </div>
+            <div className="p-5 space-y-5">
+              <h1 className="text-4xl lg:text-5xl font-bold text-center text-primary">
+                100%
+              </h1>
+              <p className="text-center text-gray-500">Sonrisas garantizadas</p>
+            </div>
+          </div>
+          <p className="text-gray-400 text-xs p-5 text-center">
+            *Datos de la guía de cálculo de emisiones de la Generalitat de
+            Cataluña y la CNMC
+          </p>
+        </div>
+
+        <div className="container px-4 py-10 mx-auto lg:flex lg:items-center lg:justify-between">
+          <div className="flex flex-col gap-3" data-aos="fade-right">
+            <h2 className="text-5xl font-bold tracking-tight text-gray-800 xl:text-6xl ">
+              ¿List@ para convertirte <br /> en un{" "}
+              <span className="text-primary">eco</span>Driver?
+            </h2>
+            <p className="text-gray-500 text-xl">
+              Te damos un toque cuando esté listo
+            </p>
+          </div>
+          <EcoDriverForm />
         </div>
       </section>
     </>
