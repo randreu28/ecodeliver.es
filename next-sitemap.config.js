a52e96cdb653afaf95e329c8fc5def47e9d0e1ed
randreu28 <randreu28@gmail.com>
2022-11-17 16:55:07 +0100

    Implementes next-sitemap
    https://github.com/iamvishnusankar/next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: process.env.SITE_URL ? process.env.SITE_URL : process.env.VERCEL_URL,
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+};
+
+module.exports = config;
+//https://github.com/iamvishnusankar/next-sitemap
