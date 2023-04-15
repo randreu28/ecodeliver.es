78e3e46fc7756f9d96d2ec514325cb4cb8c8c1f5
Rubén Chiquin <randreu28@gmail.com>
2023-04-15 18:06:09 +0200

    Adds some Cloud logos
    Some are hidden on mobile devices as they occupy too much space

--- components/CloudLogo.tsx
+++ components/CloudLogo.tsx
@@ -2,8 +2,8 @@
 export default function CloudLogo() {
   return (
     <div className="max-w-screen-xl mx-auto my-20">
-      <div className="gap-8 space-y-16 md:space-y-0 md:grid md:grid-cols-6 lg:grid-cols-5">
-        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+      <div className="gap-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+        <div className="flex items-center justify-center">
           <img
             className="grayscale max-w-full h-12"
             src="/media/cloudLogos/barcelonaActiva.png"
@@ -11,7 +11,7 @@ export default function CloudLogo() {
           />
         </div>
 
-        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+        <div className="hidden md:flex items-center justify-center">
           <img
             className="grayscale max-w-full h-12"
             src="/media/cloudLogos/caixaBank.png"
@@ -19,7 +19,7 @@ export default function CloudLogo() {
           />
         </div>
 
-        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+        <div className="flex items-center justify-center">
           <img
             className="grayscale max-w-full h-24"
             src="/media/cloudLogos/imaginBank.png"
@@ -27,7 +27,7 @@ export default function CloudLogo() {
           />
         </div>
 
-        <div className="flex items-center justify-center col-span-1 md:col-span-3 lg:col-span-1">
+        <div className="hidden md:flex items-center justify-center">
           <img
             className="grayscale max-w-full h-12"
             src="/media/cloudLogos/upc.png"
@@ -35,10 +35,50 @@ export default function CloudLogo() {
           />
         </div>
 
-        <div className="flex items-center justify-center col-span-2 md:col-span-3 lg:col-span-1">
+        <div className="hidden md:flex items-center justify-center">
           <img
             className="grayscale max-w-full h-24"
             src="/media/cloudLogos/caixaEnginyers.png"
+            alt="cloud logo"
+          />
+        </div>
+
+        <div className="hidden md:flex items-center justify-center">
+          <img
+            className="grayscale max-w-full h-12"
+            src="/media/cloudLogos/generalitat.png"
+            alt="cloud logo"
+          />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <img
+            className="grayscale max-w-full h-12"
+            src="/media/cloudLogos/logisticsIncubator.png"
+            alt="cloud logo"
+          />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <img
+            className="grayscale max-w-full h-12"
+            src="/media/cloudLogos/nextGen.png"
+            alt="cloud logo"
+          />
+        </div>
+
+        <div className="hidden md:flex items-center justify-center">
+          <img
+            className="grayscale max-w-full h-12"
+            src="/media/cloudLogos/zonaFranca.png"
+            alt="cloud logo"
+          />
+        </div>
+
+        <div className="flex items-center justify-center md:col-span-3 lg:col-span-1">
+          <img
+            className="grayscale max-w-full h-12"
+            src="/media/cloudLogos/europeanUnion.png"
             alt="cloud logo"
           />
         </div>
