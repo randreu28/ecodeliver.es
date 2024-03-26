65f1bf0fb59fc845c79d536feb6c322952a29be7
Rubén Andreu <ruben@ecodeliver.tech>
2024-03-26 14:45:51 +0100

    core: Removes the depcriated pages

--- next.config.js
+++ next.config.js
@@ -2,39 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        has: [
-          {
-            type: "cookie",
-            key: "isBusiness",
-            value: "true",
-          },
-        ],
-        permanent: false,
-        destination: "/empresa",
-      },
-      {
-        source: "/",
-        has: [
-          {
-            type: "cookie",
-            key: "isBusiness",
-            value: "false",
-          },
-        ],
-        permanent: false,
-        destination: "/particular",
-      },
-      {
-        source: "/",
-        permanent: false,
-        destination: "/particular",
-      },
-    ];
-  },
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "es",
---
+++ pages/index.tsx
@@ -0,0 +1,3 @@
+export default function SPlashScreen() {
+  return <div>SPlashScreen</div>;
+}
