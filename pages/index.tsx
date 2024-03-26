befa538ba3a0ab2d6df45d0eb830c633bcacb4d7
Rubén Andreu <ruben@ecodeliver.tech>
2024-03-26 15:36:57 +0100

    fix: paddings

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export default function Example() {
           />
         </div>
 
-        <div className="mx-auto max-w-4xl py-32 sm:py-48 lg:py-56 min-h-screen">
+        <div className="mx-auto max-w-4xl py-32 sm:py-24 lg:py-32 min-h-screen">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 flex flex-row gap-2">
               <p>{splashScreenTranslations.explorePartnerships}</p>
@@ -107,7 +107,7 @@ export default function Example() {
               {splashScreenTranslations.description}
             </p>
           </div>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
+          <div className="mt-4 flex items-center justify-center gap-x-6">
             <EcoDriverForm />
           </div>
         </div>
