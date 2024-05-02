3b08cf57eebe9664bdfe6b9653f8e9e439a2d8cd
Rubén Andreu <ruben@ecodeliver.tech>
2024-05-02 18:02:50 +0200

    (core): Adds typopgraphy tailwindcss plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "tailwindcss";
 import { ecoDeliverTheme } from "./src/utils/theme";
+import typography from "@tailwindcss/typography";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [ecoDeliverTheme],
+  plugins: [ecoDeliverTheme, typography],
 } satisfies Config;
