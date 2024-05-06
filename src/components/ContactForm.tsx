5d8da941516d3227aa48d932d4ccbe85c5b7e813
Rubén Andreu <ruben@ecodeliver.tech>
2024-05-06 09:27:42 +0200

    (fix): Contact information and typos

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -33,7 +33,7 @@ export default function ContactForm() {
       <div className="container px-6 py-12 mx-auto">
         <div className="lg:flex lg:items-center lg:-mx-6">
           <div className="lg:w-1/2 lg:mx-6">
-            <h1 className="text-5xl font-semibold text-gray-800 capitalize dark:text-white lg:text-3xl">
+            <h1 className="text-4xl font-semibold text-gray-800 capitalize dark:text-white lg:text-6xl">
               ¡Contáctanos!
             </h1>
 
@@ -45,30 +45,11 @@ export default function ContactForm() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                  />
-                </svg>
-
-                <span className="mx-2 truncate w-72">+34 666 666 666</span>
-              </p>
-
-              <p className="flex items-start -mx-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mx-2 text-primary"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
@@ -109,7 +90,7 @@ export default function ContactForm() {
                     htmlFor="email"
                     className="block mb-2 text-sm text-gray-600 dark:text-gray-200"
                   >
-                    Email address
+                    Correo electrónico
                   </label>
                   <input
                     required
