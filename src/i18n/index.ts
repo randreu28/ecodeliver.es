ed41fa2a7215a1c7d3eb6551de635bc45d4fc892
Rubén Andreu <ruben@ecodeliver.tech>
2024-05-29 15:49:09 +0200

    (fix): Makes use of the i18n routes on <Footer/> and some other places

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,21 +1,21 @@
-import english from './en.json';
-import spanish from './es.json';
+import english from "./en.json";
+import spanish from "./es.json";
 
 /**
- * Retrieves the internationalization object based on the provided locale. 
+ * Retrieves the internationalization object based on the provided locale.
  * Uses web standards for locale names.
- * 
+ *
  * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language#syntax
  * @param locale The locale identifier.
  * @returns The localization resources for the specified locale.
  */
-export function getI18N(locale= "es") {
-	switch (locale) {
-		case 'en':
-			return english;
-		case 'es':
-			return spanish;
-        default:
-            return spanish;
-	}
+export function getI18N(locale = "es") {
+  switch (locale) {
+    case "en":
+      return english;
+    case "es":
+      return spanish;
+    default:
+      return spanish;
+  }
 }
