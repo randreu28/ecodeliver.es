fc40b7d493c98bba8c29e7fed5d49a468d51bcdb
Rubén Andreu <ruben@ecodeliver.tech>
2024-05-30 10:48:01 +0200

    (core): Adds i18n to /collaborative

--- src/components/EmissionBar.tsx
+++ src/components/EmissionBar.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { getI18N } from "@/i18n";
 
-export default function EmissionBar() {
+type Props = {
+  i18n: ReturnType<typeof getI18N>["collaborative"]["emissionBar"];
+};
+
+export default function EmissionBar({ i18n }: Props) {
   const DIESEL_EMISSIONS = 67.4; // grams of CO2 per package using diesel trucks
   const ECO_EMISSIONS = 3.5; // grams of CO2 per package using ecoDrivers
   const RANGE_MIN = 0;
@@ -15,36 +20,33 @@ export default function EmissionBar() {
 
   return (
     <section className="container my-32 space-y-5">
-      <h2 className="text-4xl text-center font-bold">
-        Imagina un futuro con nosotros
-      </h2>
+      <h2 className="text-4xl text-center font-bold">{i18n.title}</h2>
       <p className="text-primary text-6xl font-bold text-center">
-        {emissionsSaved} kg de CO2
+        {emissionsSaved} kg {i18n.of} CO2
       </p>
 
       <p className="text-muted-foreground text-center max-w-2xl mx-auto">
-        Ahorrados al usar el modelo colaborativo en vez de camiones diesel
-        convencionales, de acuerdo con la{" "}
+        {i18n.accordingTo}{" "}
         <a
           className="hover:underline text-primary"
           href="https://canviclimatic.gencat.cat/es/actua/guia_de_calcul_demissions_de_co2/"
           target="_blank"
         >
-          guía de cálculo de emisiones de la Generalitat de Cataluña
+          {i18n.ref1}
         </a>{" "}
-        y{" "}
+        {i18n.and}{" "}
         <a
           href="https://www.transportes.gob.es/carreteras/sostenibilidad/eee-2030"
           target="_blank"
           className="hover:underline text-primary"
         >
-          el ministerio de transporte y sostenibilidad español
+          {i18n.ref2}
         </a>
       </p>
 
       <div className="">
         <label htmlFor="packageRange" className="sr-only">
-          Numero de paquetes
+          {i18n.packageQuantity}
         </label>
         <input
           id="packageRange"
@@ -56,7 +58,7 @@ export default function EmissionBar() {
           className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer slider"
         />
         <h2 className="mt-2 text-center text-lg font-semibold">
-          {packages.toLocaleString()} paquetes
+          {packages.toLocaleString()} {i18n.packages}
         </h2>
       </div>
     </section>
